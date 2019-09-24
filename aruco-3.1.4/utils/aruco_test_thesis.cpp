09e62d51e043673c8e9171cfd240891e678772fe
dayaen1b <enes.dayangac@cmore-automotive.com>
2019-09-24 23:49:35 +0200

    Revert "euler angles conversion"
    This reverts commit 9034155992c594e3fd54000b62ff3971488353c6.

--- aruco-3.1.4/utils/aruco_test_thesis.cpp
+++ aruco-3.1.4/utils/aruco_test_thesis.cpp
@@ -1,4 +1,4 @@
- #include "aruco.h"
+#include "aruco.h"
 #include "cvdrawingutils.h"
 
 #include <fstream>
@@ -9,14 +9,32 @@
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include "aruco_test_thesis.h"
+#include <assert.h>
 
-struct   TimerAvrg { std::vector<double> times;
-size_t curr = 0, n; std::chrono::high_resolution_clock::time_point begin, end;   TimerAvrg(int _n = 30) 
-{ n = _n; times.reserve(n); }inline void start()
-{ begin = std::chrono::high_resolution_clock::now(); }inline void stop()
-{ end = std::chrono::high_resolution_clock::now(); double duration = double(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count())*1e-6; if (times.size() < n) times.push_back(duration); else { times[curr] = duration; curr++; if (curr >= times.size()) curr = 0; } }double getAvrg()
-{ double sum = 0; for (auto t : times) sum += t; return sum / double(times.size()); } };
+struct   TimerAvrg {
+    std::vector<double> times;
+    size_t curr = 0, n; std::chrono::high_resolution_clock::time_point begin, end;   TimerAvrg(int _n = 30)
+    {
+        n = _n; times.reserve(n);
+    }inline void start()
+    {
+        begin = std::chrono::high_resolution_clock::now();
+    }inline void stop()
+    {
+        end = std::chrono::high_resolution_clock::now(); double duration = double(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count())*1e-6; if (times.size() < n) times.push_back(duration); else { times[curr] = duration; curr++; if (curr >= times.size()) curr = 0; }
+    }double getAvrg()
+    {
+        double sum = 0; for (auto t : times) sum += t; return sum / double(times.size());
+    }
+};
 
+cv::Mat rot2euler(cv::Mat dest);
+cv::Mat convert4x4to3x3(cv::Mat T);
+cv::Vec3f rotationMatrixToEulerAngles(cv::Mat cam0_id113x3);
+//void rotationMatrixToEulerAngles(cv::Mat dest);
 
 int main(int argc, char** argv)
 {
@@ -78,7 +96,7 @@ int main(int argc, char** argv)
         markers_1[i].draw(image_1, cv::Scalar(0, 0, 255), 2, true);
     }
 
-    
+
 
     /* draw a 3d cube in each marker if there is 3d info */
     if (camparams_1.isValid() && markerSize_1 > 0) {
@@ -90,15 +108,160 @@ int main(int argc, char** argv)
 
     /* TODO: check if detected */
     cv::Mat T_cam0_id11 = markers_0[0].getTransformMatrix();
+
+    //conver cam0_id11 to 3*3
+    cv::Mat cam0_id113x3 = convert4x4to3x3(T_cam0_id11);
+    cv::Vec3f xyz = rotationMatrixToEulerAngles(cam0_id113x3.t());
+    std::cout << xyz;
     cv::Mat T_cam0_id17 = markers_0[1].getTransformMatrix();
+    cv::Vec3f xyz17 = rotationMatrixToEulerAngles(T_cam0_id17);
+    std::cout << xyz17;
     cv::Mat T_cam1_id17 = markers_1[0].getTransformMatrix();
 
     /* GT: dist btw cam and marker: 1710.77 mm , detection: marker wrt. camera */
-    cv::Mat T = T_cam0_id11.inv() * T_cam0_id17 * T_cam1_id17.inv();
+    cv::Mat RmarkerOnTheCar = cv::Mat::eye(3, 3, CV_32FC1);
+    cv::Mat TmarkerOnTheCar = cv::Mat::eye(4, 4, CV_32FC1);
+    // TODO: put 0,90,0 from blender to obtain R, later T with the
+    TmarkerOnTheCar.at<float>(0, 3) = 0.168206;
+    TmarkerOnTheCar.at<float>(1, 3) = 1.42624;
+    TmarkerOnTheCar.at<float>(2, 3) = 1.38777;
+    cv::Mat T = TmarkerOnTheCar * T_cam0_id11.inv() * T_cam0_id17 * T_cam1_id17.inv();
+    //camera on car location ground truth [m]
+    float gx = -0.30904;
+    float gy = 3.0766;
+    float gz = 1.8331;
+    float ex = std::abs(T.at<float>(0, 3) - gx);
+    float ey = std::abs(T.at<float>(1, 3) - gy);
+    float ez = std::abs(T.at<float>(2, 3) - gz);
 
-    float dist = sqrt(T.at<float>(0, 3) * T.at<float>(0, 3) +
-        T.at<float>(1, 3) * T.at<float>(1, 3) + 
+    float dist = sqrt(T.at<float>(0, 3) * T.
+        at<float>(0, 3) +
+        T.at<float>(1, 3) * T.at<float>(1, 3) +
         T.at<float>(2, 3) * T.at<float>(2, 3));
 
+    /*
+    std::cout << "dest" << dest;
+    cv::Mat Eulerangles_id17_cam_0 = cv::Mat(3, 3, CV_32FC1);
+    cv::Mat dest = convert4x4to3x3(T_cam0_id17);
+    Eulerangles_id17_cam_0 = rot2euler(dest);
+    rotationMatrixToEulerAngles(dest);
+
+    std::cout << Eulerangles_id17_cam_0;
+    cv::Mat Degree_euler = Eulerangles_id17_cam_0 * 180 / CV_PI;
+    std::cout << Degree_euler;
+    cv::Mat Eulerangles_id11 = cv::Mat(3, 3, CV_32FC1);
+
+
+     dest = convert4x4to3x3(T);
+    Eulerangles_id11 = rot2euler(dest);
+    std::cout << Eulerangles_id11; */
     return 0;
+
 }
+
+cv::Mat convert4x4to3x3(cv::Mat mat4x4)
+{
+    int width = mat4x4.cols;
+    int height = mat4x4.rows;
+    cv::Mat rota = cv::Mat(3, 3, CV_32FC1);
+    for (int x = 0; x < height - 1; x++)
+    {
+        for (int y = 0; y < width - 1; y++)
+        {
+
+            rota.at<float>(x, y) = mat4x4.at<float>(x, y);
+
+        }
+    }
+    return rota;
+}
+
+cv::Vec3f rotationMatrixToEulerAngles(cv::Mat R)
+{
+
+    //assert(isRotationMatrix(R));
+
+    float sy = sqrt(R.at<float>(0, 0) * R.at<float>(0, 0) + R.at<float>(1, 0) * R.at<float>(1, 0));
+
+    bool singular = sy < 1e-6; // If
+
+    float x, y, z;
+    if (!singular)
+    {
+        x = atan2(R.at<float>(2, 1), R.at<float>(2, 2));
+        y = atan2(-R.at<float>(2, 0), sy);
+        z = atan2(R.at<float>(1, 0), R.at<float>(0, 0));
+    }
+    else
+    {
+        x = atan2(-R.at<float>(1, 2), R.at<float>(1, 1));
+        y = atan2(-R.at<float>(2, 0), sy);
+        z = 0;
+    }
+    return cv::Vec3f(x, y, z);
+
+
+
+}
+/*
+ cv::Mat rot2euler(cv::Mat dest)
+{
+     //std::cout << dest ;
+
+    cv::Mat euler(3, 1, CV_32FC1);
+    double m00 = dest.at<float>(0, 0);
+    double m02 = dest.at<float>(0, 2);
+    double m10 = dest.at<float>(1, 0);
+    double m11 = dest.at<float>(1, 1);
+    double m12 = dest.at<float>(1, 2);
+    double m20 = dest.at<float>(2, 0);
+    double m22 = dest.at<float>(2, 2);
+    double x, y, z;
+
+    // Assuming the angles are in radians.
+    if (m10 > 0.998) { // singularity at north pole
+        x = 0;
+        y = CV_PI / 2;
+        z = atan2(m02, m22);
+    }
+    else
+    {
+        x = atan2(-m12, m11);
+        y = asin(m10);
+        z = atan2(-m20, m00);
+    }
+
+    euler.at<float>(0) = x;
+    euler.at<float>(1) = y;
+    euler.at<float>(2) = z;
+
+    return euler;
+}
+
+ void rotationMatrixToEulerAngles(cv::Mat R)
+ {
+     std::cout << R;
+     float sy = sqrt(R.at<float>(0, 0) * R.at<float>(0, 0) + R.at<float>(1, 0) * R.at<float>(1, 0));
+
+     bool singular = sy < 1e-6; // If
+
+     float x, y, z;
+     if (!singular)
+     {
+         x = atan2(R.at<float>(2, 1), R.at<float>(2, 2));
+         y = atan2(-R.at<float>(2, 0), sy);
+         z = atan2(R.at<float>(1, 0), R.at<float>(0, 0));
+     }
+     else
+     {
+         x = atan2(-R.at<float>(1, 2), R.at<float>(1, 1));
+         y = atan2(-R.at<float>(2, 0), sy);
+         z = 0;
+     }
+     std::cout << "x" << x << "y" << y << "z" << z;
+
+
+
+
+ }
+ */
